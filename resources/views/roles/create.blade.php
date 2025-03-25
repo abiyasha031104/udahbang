63837e256663a5dd1d98bd14b21d66548428eb3b
alghazali <h1101231059@student.untan.ac.id>
2025-03-25 19:39:28 +0700

    update-roles

--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -48,6 +48,6 @@
         </div>
     </div>
 </form>
-<p class="text-center text-primary"><small>E-Commerce</small></p>
+<p class="text-center text-primary"><small>Powered by Larisin Aja Mart</small></p>
 
 @endsection
